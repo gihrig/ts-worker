9c817d5be56373a86696641f42a7a01587c7cba4
gihrig <glen@arkadias.com>
2024-05-15 12:38:35 -0700

    feat(index.ts): replace hello example with CF/AI - WIP
    - Broken. followed video, copied source from github
    - Error: Could not resolve "base64-js"
    - You can mark the path "base64-js" as external to exclude it from the bundle, which will remove this error.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Ai } from '@cloudflare/ai'
+import { Hono } from 'hono'
 
-import { Hono } from 'hono';
+export interface Env {
+  AI: any
+}
 
-export interface Env { }
+const app = new Hono<{ Bindings: Env }>()
 
-const app = new Hono<{ Bindings: Env }>();
+// GET /?query="How is your day today?"
+app.get("/", async c => {
+  const ai = new Ai(c.env.AI)
 
-app.get('/', c => c.json({ 'hello': 'world!' }));
+  const content = c.req.query("query") || 'What is the origin of the phrase Hello, World'
 
-export default app;
+  const messages = [
+    { role: 'system', content: 'You are a friendly assistant' },
+    { role: 'user', content }
+  ];
+
+  const inputs = { messages }
+
+  const res = await ai.run("@cf/mistral/mistral-7b-instruct-v0.1", inputs)
+
+  return c.json(res)
+})
+
+export default app
